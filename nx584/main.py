65d0d3ebb4da55b9596f3009513be5ac0e48212a
Jamie Hopper <jamie@mhztech.com>
2020-05-19 08:41:11 -0500

    Update main.py

--- nx584/main.py
+++ nx584/main.py
@@ -1,23 +1,16 @@
 import argparse
-import logging
-import logging.handlers
 import os
 import threading
 
-from nx584 import api
-from nx584 import controller
-
-LOG_FORMAT = '%(asctime)-15s %(module)s %(levelname)s %(message)s'
-LOG_LEVEL = logging.INFO
-SYSLOG_HANDLER = logging.handlers.SysLogHandler(address = '/dev/log')
+#custom standardized logging
+from config import LOGGER as LOG
+LOG.info('starting pynx584 controller')
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--config', default='config.ini',
                         metavar='FILE',
                         help='Path to config file')
-    parser.add_argument('--debug', default=False, action='store_true',
-                        help='Enable debug')
     parser.add_argument('--connect', default=None,
                         metavar='HOST:PORT',
                         help='Host and port to connect for serial stream')
@@ -34,34 +27,7 @@ def main():
                         help='Listen port (defaults to 5007)')
     args = parser.parse_args()
 
-    LOG = logging.getLogger()
-    LOG.setLevel(logging.DEBUG)
-    formatter = logging.Formatter(LOG_FORMAT)
-    istty = os.isatty(0)
-
-    if args.debug and not istty:
-        debug_handler = logging.handlers.RotatingFileHandler(
-            'debug.log',
-            maxBytes=1024*1024*10,
-            backupCount=3)
-        debug_handler.setFormatter(formatter)
-        debug_handler.setLevel(logging.DEBUG)
-        LOG.addHandler(debug_handler)
-
-    if istty:
-        verbose_handler = logging.StreamHandler()
-        verbose_handler.setFormatter(formatter)
-        verbose_handler.setLevel(args.debug and logging.DEBUG or logging.INFO)
-        LOG.addHandler(verbose_handler)
-
-    if SYSLOG_HANDLER:
-        log_handler = SYSLOG_HANDLER
-        log_handler.setFormatter(formatter)
-        log_handler.setLevel(LOG_LEVEL)
-        LOG.addHandler(log_handler)
-
-    LOG.info('Ready')
-    logging.getLogger('connectionpool').setLevel(logging.WARNING)
+    LOG.info('Client Ready')
 
     if args.connect:
         host, port = args.connect.split(':')
