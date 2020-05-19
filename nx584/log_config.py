f5169edf7a403ffb79014008d2e181fdfbf6128f
Jamie Hopper <jamie@mhztech.com>
2020-05-19 10:10:11 -0500

    Update log_config.py

--- nx584/log_config.py
+++ nx584/log_config.py
@@ -5,25 +5,23 @@ import logging.handlers
 __VERSION__ = '0.0.4'
 LOG_FORMAT = '%(asctime)-15s %(module)s %(levelname)s %(message)s'
 SYSLOG_HANDLER = logging.handlers.SysLogHandler(address = '/dev/log')
-SYSLOG_LEVEL = logging.INFO
 STREAM_HANDLER = logging.StreamHandler()
-STREAM_LEVEL = logging.DEBUG
     
 LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.DEBUG)
 formatter = logging.Formatter(LOG_FORMAT)
 
 syslog = SYSLOG_HANDLER
 syslog.setFormatter(formatter)
-syslog.setLevel(SYSLOG_LEVEL)
+syslog.setLevel(logging.INFO)
 LOGGER.addHandler(syslog)
 
 istty = os.isatty(0)
 if istty:
   stream = STREAM_HANDLER
   stream.setFormatter(formatter)
-  stream.setLevel(STREAM_LEVEL)
+  stream.setLevel(logging.DEBUG)
   LOGGER.addHandler(stream)
-
   LOGGER.debug(f'Stream ready - pynx584 v{__VERSION__}')
 
 LOGGER.info(f'pynx584 v{__VERSION__} ready')
